3898e529ef0fde9f70d8b213e7755e9aad792b8a
SharCom <mfarhanakhtar48@gmail.com>
2021-12-29 01:12:57 -0800

    init

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.blue,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(children: [
+        Expanded(
+          child: FlatButton(
+              onPressed: () {
+                print(
+                  'left button got pressed ',
+                );
+              },
+              child: Image.asset('images/dice1.png')),
+        ),
+        Expanded(
+          child: FlatButton(
+            child: Image.asset('images/dice1.png'),
+          ),
+        ),
+      ]),
+    );
+  }
+}
